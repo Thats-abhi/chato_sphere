eb63cd38e43cc47b3dc0a617800778a2893b6bb4
Abhishek Choudhary <136879950+Thats-abhi@users.noreply.github.com>
2024-08-24 01:38:19 +0530

    Update2  server.js

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler); 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
